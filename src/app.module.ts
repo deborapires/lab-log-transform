4e34d47064ab2752cd7ac118d76a5ebf3b94b257
Debora Pires <deborapires@Deboras-MacBook-Air.local>
2025-05-14 23:47:26 -0300

    update modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
-import { UserOrderController } from './controllers/user-order.controller';
-import { UserOrderService } from './services/user-order.service';
+import { UserOrderController } from './controllers/user-order.controller'; // Importe o controller
+import { UserOrderService } from './services/user-order.service'; // Importe o service
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserOrderEntity } from './entities/userOrderEntity';
-
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserOrderEntity]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER || 'appuser',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'postgres',
+      entities: [UserOrderEntity],
+      synchronize: true, 
+    }),
+    MulterModule.register({
+      dest: './uploads',
+    }),
   ],
   controllers: [UserOrderController],
   providers: [UserOrderService],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Lab Log Transform API')
+    .setDescription('API for managing user orders')
+    .setVersion('1.0')
+    .addTag('userOrder')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
   console.log(`Application is running on port: ${port}`);
